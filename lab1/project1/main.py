3bcf70eaff47e4017b379f83caf7267c68006ea8
Tikhonov-Roman <103669215+Tikhonov-Roman@users.noreply.github.com>
2025-11-05 21:29:49 +0300

    Create main.py

---
+++ lab1/project1/main.py
@@ -0,0 +1,16 @@
+from starlette.applications import Starlette
+from starlette.responses import HTMLResponse
+from starlette.routing import Route
+
+async def home(request):
+    return HTMLResponse(f"""<pre>
+    Project 1
+    url: {str(request.url)}
+    client:
+        host: {request.client.host}
+        port: {request.client.port}
+    headers:
+        {('<br>' + '&nbsp' * 12).join(k + ': ' + v for k, v in request.headers.items())}
+</pre>""")
+
+app = Starlette(debug=True, routes=[Route('/', home)])
